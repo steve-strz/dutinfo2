a28391212afa4f75c6b658cb137761e9557b5dee
Steve STRAZZERI <pro.stevestrz@gmail.com>
2020-09-25 10:16:44 +0200

    for chris

--- Prog web/A314/tp1/index.php
+++ Prog web/A314/tp1/index.php
@@ -1,15 +1,16 @@
-<?php
-    $x = 2;
-    $y = 4;
-    $z = 0;
-    $msg = "Bonjour à tous";
-    define("constante", "i'm the constante");
-?>
+
 <!DOCTYPE html>
 <html lang="fr">
     <head>
         <meta charset="UTF-8"/>
         <title> Mon TD N°1</title>
+        <?php
+            $x = 2;
+            $y = 4;
+            $z = 0;
+            $msg = "Bonjour à tous";
+            define("constante", "i'm the constante");
+        ?>
     </head>
     <body>
         <?php
@@ -21,7 +22,21 @@
             $cdc1 = "test";
             $cdc2 = 'test';
             //3.2
-            echo "\"Je m'appelle Martin\"";
+            echo "\"Je m'appelle Martin\" <br/>";
+            //3.3
+            echo strlen('utilisation de la fonction strlen');
+            echo '<br/>';
+            echo strpos('utilisation de la fonction strpos', 'utilisation');
+            echo '<br/>';
+            echo strpos('utilisation de la fonction strpos', 'fonction');
+            echo '<br/>';
+            echo strstr('utilisation de la fonction strstr', 'la');
+            echo '<br/>';
+            echo substr('utilisation de la fonction substr', -4);
+            echo '<br/>';
+            echo str_replace('test', 'coucou', 'ceci est un test');
+            echo '<br/>';
+            echo html_entity_decode('<b>ceci est un test</b>');
         ?>
     </body>
 </html>
